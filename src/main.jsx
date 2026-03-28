bec97a28cffe6b35e8d43c935be0fa9d3286d17d
Sharveshraja1009 <sharveshraja1009@gmail.com>
2026-03-28 15:51:13 +0530

    Add routes for Privacy Policy, About Us, Terms and Conditions, and Search pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,10 @@ import Home from './home.jsx'
 import Contact from './ContactUs.jsx'
 import Registration from './Registration.jsx'
 import Detail from './Detail.jsx'
+import PrivacyPolicy from './PrivacyPolicy.jsx'
+import AboutUs from './AboutUs.jsx'
+import TermsAndConditions from './TermsAndConditions.jsx'
+import Search from './search.jsx'
 import './index.css'
 import './i18n.js'
 
@@ -18,6 +22,10 @@ function App() {
         <Route path="/registration" element={<Registration />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
     </div>
   )
